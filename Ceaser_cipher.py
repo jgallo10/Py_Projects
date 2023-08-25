ac4760dfb8f2993f5fea9bfc9656e51998939a3e
jgallo10 <77376034+jgallo10@users.noreply.github.com>
2023-08-24 23:12:54 -0500

    finished up encryption fucntion, added decryption function, added some error handaling for directoin

--- Ceaser_cipher.py
+++ Ceaser_cipher.py
@@ -5,18 +5,39 @@ alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
 text = input("Type your message: \n").lower()
 shift = int(input("Type the shift number: \n"))
+alphabet_length = len(alphabet)
 
+#Create fucntion
 def encrypt(text = text, shift = shift):
+    
+    #Create empty string to input cipher 
     encrypted = ""
-    differance = 26
-    if shift > differance:
-        while shift > differance:
-            differance += differance
-    shift = differance - shift
     
-    for positon in range(len(text)):
-        letter_index = alphabet.index(text[positon]) + shift
+    #loop to encrypt the text input
+    for position in range(len(text)):
+        letter_index = alphabet.index(text[position]) + shift
+        while letter_index > alphabet_length:
+            letter_index -= alphabet_length
         encrypted += alphabet[letter_index]
-    print(f"The encoded text is {encrypted}")
+    print(f"The encoded text is {encrypted} \n")
+
+
+
+def decrypt(text = text, shift = shift):
     
-encrypt(text, shift)
+    decryption = ""
+    for position in range(len(text)):
+        letter_index = alphabet.index(text[position]) - shift
+        while letter_index < 0:
+            letter_index += alphabet_length
+        decryption += alphabet[letter_index]
+    print(f"The decoded text is {decryption} \n")
+    
+    
+if direction == "encode":
+    encrypt(text, shift)
+elif direction == "decode":
+    decrypt(text, shift)
+else:
+    print("Sorry you have to pick 'encode' or 'decode'.")
+    

11c84456fdb9a9d656fe2d264983c5e268e33467
jgallo10 <77376034+jgallo10@users.noreply.github.com>
2023-08-25 00:04:07 -0500

    added ceaser fucntion (shorter version of previous code) in order to get rid of recundancy

--- Ceaser_cipher.py
+++ Ceaser_cipher.py
@@ -5,9 +5,36 @@ alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
 text = input("Type your message: \n").lower()
 shift = int(input("Type the shift number: \n"))
-alphabet_length = len(alphabet)
 
-#Create fucntion
+
+
+def ceaser(begin_text, amount_shifted, which_cipher):
+    final_text = ""
+    alphabet_length = len(alphabet)
+    if which_cipher == "decode":
+            amount_shifted *= -1
+            
+    for letter in begin_text:
+
+        letter_index = alphabet.index(letter) + amount_shifted
+        
+        if letter_index > alphabet_length or letter_index < 0:
+            letter_index = letter_index % alphabet_length
+        
+        final_text += alphabet[letter_index]
+        
+    print(f"The final text is {final_text} \n")
+        
+        
+ceaser(begin_text=text, amount_shifted=shift, which_cipher=direction )
+        
+
+
+
+
+
+#..................................................Longer version..........................................................................
+""" #Create fucntion
 def encrypt(text = text, shift = shift):
     
     #Create empty string to input cipher 
@@ -40,4 +67,4 @@ elif direction == "decode":
     decrypt(text, shift)
 else:
     print("Sorry you have to pick 'encode' or 'decode'.")
-    
+     """

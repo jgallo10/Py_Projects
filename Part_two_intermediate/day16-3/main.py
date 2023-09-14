9abf60fa423721e2c75b95a7e977c8db276d2155
jgallo10 <77376034+jgallo10@users.noreply.github.com>
2023-09-14 01:03:04 -0500

    commented out turtle code, installed prettytable with pip and processing how to use it

--- Part_two_intermediate/day16-3/main.py
+++ Part_two_intermediate/day16-3/main.py
@@ -1,14 +1,22 @@
+# import another_mod
 
-import another_mod
+# print(another_mod.another_variable)
 
-print(another_mod.another_variable)
+# from turtle import Turtle, Screen
 
-from turtle import Turtle, Screen
+# ben = Turtle()
+# ben.shape("turtle")
+# ben.color("blue")
+# ben.forward(100)
 
-ben = Turtle()
-ben.shape("turtle")
-ben.color("blue")
-ben.forward(100)
 
-my_screen = Screen()
-my_screen.exitonclick()
+# my_screen = Screen()
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+print(table)

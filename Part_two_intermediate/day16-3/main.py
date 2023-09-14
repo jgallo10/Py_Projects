252fb3005c5b76354e6802fefc90c28e1943bb52
jgallo10 <77376034+jgallo10@users.noreply.github.com>
2023-09-14 00:36:07 -0500

    adding folder/ files, OOP with turtle

---
+++ Part_two_intermediate/day16-3/main.py
@@ -0,0 +1,14 @@
+
+import another_mod
+
+print(another_mod.another_variable)
+
+from turtle import Turtle, Screen
+
+ben = Turtle()
+ben.shape("turtle")
+ben.color("blue")
+ben.forward(100)
+
+my_screen = Screen()
+my_screen.exitonclick()

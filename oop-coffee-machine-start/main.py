48150e497b055f8556d38d21a2ab89a88b80c333
jgallo10 <77376034+jgallo10@users.noreply.github.com>
2023-09-26 23:37:24 -0500

    modified p2 file. added starter code for coffee machine project.

---
+++ oop-coffee-machine-start/main.py
@@ -0,0 +1,7 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+def coffe_machine():
+    coffe_type = input("What would you like? (expresso/latte/cappuccino)")
+    
